bd8273724901cbc8ce360e4b234fb5a6e36a5ec4
baba-s <shotababa1206@gmail.com>
2022-11-20 11:42:47 +0900

    add

---
+++ Editor/SaveDefinesToTextScope.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using UnityEditor;
+
+namespace Kogane
+{
+    /// <summary>
+    /// ビルドに使用した defines の情報を Resources フォルダのテキストファイルに書き込むエディタ拡張
+    /// </summary>
+    public sealed class SaveDefinesToTextScope : IDisposable
+    {
+        //================================================================================
+        // 変数(static readonly)
+        //================================================================================
+        private static readonly string DIRECTORY_NAME = $"Assets/{nameof( SaveDefinesToTextScope )}/Resources";
+        private static readonly string FILE_NAME      = "defines.txt";
+
+        //================================================================================
+        // 関数
+        //================================================================================
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public SaveDefinesToTextScope
+        (
+            bool   isReleaseBuild,
+            string defines
+        )
+        {
+            // リリースビルドにテキストファイルが含まれないように
+            // ビルド開始時に削除しています
+            Refresh();
+
+            if ( isReleaseBuild ) return;
+
+            var text = string.Join( "\n", defines.Split( ";" ) );
+
+            Directory.CreateDirectory( DIRECTORY_NAME );
+            var path = $"{DIRECTORY_NAME}/{FILE_NAME}";
+            File.WriteAllText( path, text, Encoding.UTF8 );
+            AssetDatabase.ImportAsset( path );
+        }
+
+        /// <summary>
+        /// ビルド終了時に呼び出されます
+        /// </summary>
+        public void Dispose()
+        {
+            Refresh();
+        }
+
+        /// <summary>
+        /// 作成したテキストファイルを削除します
+        /// </summary>
+        private static void Refresh()
+        {
+            var directoryName = Path.GetDirectoryName( DIRECTORY_NAME );
+            if ( !AssetDatabase.IsValidFolder( directoryName ) ) return;
+            AssetDatabase.DeleteAsset( directoryName );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.SaveDefinesToTextScope
+# Kogane Save Defines To Text Scope
+
+ビルド時に defines の情報を Resources フォルダのテキストファイルに書き込むエディタ拡張
